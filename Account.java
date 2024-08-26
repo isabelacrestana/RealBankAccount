3b1d25df12e9e0f9943eb16247a0fd40ac8268c6
isabelacrestana <crestanaisabela@gmail.com>
2024-08-26 16:14:46 -0300

    Código aula 26-08

---
+++ Account.java
@@ -0,0 +1,124 @@
+public class Account
+{
+    private long numAccount;
+    private String accountHolder;
+    private double balance;
+    private double limit;
+    private String password;
+    private static long nextNumber;
+
+    public Account()
+    {
+        this.accountHolder = "Unknown";
+        this.balance = 0.0;
+        this.password = "0000";
+        this.numAccount = ++nextNumber;
+    }
+
+    public Account (String accountHolder, long numAccount, double limit)
+    {
+        this.accountHolder = accountHolder;
+        this.numAccount = numAccount;
+        this.limit = limit;
+
+        this.balance = 0.0;
+        this.password = "0000";
+    }
+
+    public long getNumAccount()
+    {
+        return numAccount;
+    }
+
+    public void setNumAccount(long numAccount)
+    {
+        this.numAccount = numAccount;
+    }
+
+    public String getAccountHolder()
+    {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder)
+    {
+        this.accountHolder = accountHolder;;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    // public void setBalance(double balance)
+    // {
+    //     this.balance = balance;
+    // }
+
+    public double getLimit()
+    {
+        return limit;
+    }
+
+    public void setLimit(double limit)
+    {
+        this.limit = limit;
+    }
+
+    public void withdraw(double value)
+    {
+        if (value > 0)
+        {
+            
+            if (value <= balance || value <= limit)
+            {
+                this.balance -= value;
+                System.out.println("Success!");
+            }
+
+            else
+            {
+                System.out.println("Fail!");
+            }
+        }
+    }
+
+    public void deposit(double value)
+    {
+        if (value > 0)
+        {
+            this.balance += value;
+            System.out.println("Success!\n");
+        }
+
+        else
+        {
+            System.out.println("Fail!\n");
+        }
+    }
+
+    public void changePassword(String oldPassword, String newPassword)
+    {
+        if(oldPassword.equals(this.password))
+        {
+            this.password = newPassword;
+            System.out.println("Senha alterada com sucesso!");
+        }
+
+        else 
+        {
+            System.out.println("Senha incorreta. Senha não pode ser alterada!");
+        }
+
+    }
+
+    public void printAccount()
+    {
+        System.out.printf("\nAccount %04d:"+
+                          "\nAccount Holder: %s"+
+                          "\nAccount Balance: $%.2f"+
+                          "\nAccount Limit: $%.2f\n", 
+                          getNumAccount(), getAccountHolder(),
+                          getBalance(), getLimit());
+    }
+}
---
+++ AccountTest2.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class AccountTest2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int option;
+
+        Account conta3 = new Account();
+        Account conta2 = new Account("Isabela", 1234, 1000);
+        Account conta1 = new Account("Mauricio", 5, 5);
+
+        conta1.printAccount();
+        conta2.printAccount();
+        conta3.printAccount();   
+
+        do {
+            System.out.printf("\n\n" + 
+                              "<<<<<<<<<<<<<<>>>>>>>>>>>>>" +
+                              "\nMENU:\n" + 
+                              " 1. Print account data\n" + 
+                              " 2. Update password\n" + 
+                              " 3. Withdraw\n" + 
+                              " 4. Make a deposit\n" +
+                              " 0. Exit\n");
+
+            option = input.nextInt();
+
+            switch (option)
+            {
+                case 1:
+                {
+                    conta1.printAccount();
+                    break;
+                }
+
+                case 2:
+                {
+                    input.nextLine();
+
+                    System.out.println("Old password: ");
+                    String oldPassword = input.nextLine();
+
+                    System.out.println("New password: ");
+                    String newPassword = input.nextLine();
+
+                    conta1.changePassword(oldPassword, newPassword);  
+
+                    break;            
+                }
+
+                case 3:
+                {
+                    System.out.println("Withdraw value: ");
+                    double withdrawValue = input.nextDouble();
+                    conta1.withdraw(withdrawValue);
+                    break;
+                }
+
+                case 4:
+                {
+                    System.out.println("Deposit value: ");
+                    double depositValue = input.nextDouble();
+                    conta1.deposit(depositValue);
+                    break;
+                }
+
+                case 0:
+                {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                default:
+                {
+                    System.out.println("Invalid option...\n");
+                }
+            }
+        } while(option != 0);    
+
+        System.out.println();
+        input.close();
+    }
+}
